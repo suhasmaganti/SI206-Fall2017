579c4f2da448e50ac09a379282a859ae212de947
suhasmaganti <smaganti@umich.edu>
2017-12-03 23:15:09 -0500

    Add files via upload
    Initial files for final project

---
+++ Final Project/FinalProject.py
@@ -0,0 +1,13 @@
+## SI 206 2017
+## Final Project
+## Name: Suhas Maganti
+
+import json
+import sqlite3
+import facebook_info
+
+##### Authentication SETUP CODE:
+consumer_key = facebook_info.consumer_key
+consumer_secret = facebook_info.consumer_secret
+access_token = facebook_info.access_token
+access_token_secret = facebook_info.access_token_secret
